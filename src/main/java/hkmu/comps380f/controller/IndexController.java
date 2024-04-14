56d8804e3ff256d7444baf9f6b7b9f93290faa3f
MrPotato9048 <kting9048@gmail.com>
2024-04-14 16:49:48 +0800

    Modified IndexController

--- src/main/java/hkmu/comps380f/controller/IndexController.java
+++ src/main/java/hkmu/comps380f/controller/IndexController.java
@@ -34,59 +34,6 @@ public class IndexController {
         return new ModelAndView("userRegister", "appUser", new UserManagementController.UserForm());
     }
 
-    public static class UserForm {
-        private String username;
-        private String password;
-        private String role;
-        private String fullName;
-        private String emailAddress;
-        private String deliveryAddress;
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getRole() {return role;}
-
-        public void setRole(String role) {this.role = role;}
-
-        public String getFullName() {
-            return fullName;
-        }
-
-        public void setFullName(String fullName) {
-            this.fullName = fullName;
-        }
-
-        public String getEmailAddress() {
-            return emailAddress;
-        }
-
-        public void setEmailAddress(String emailAddress) {
-            this.emailAddress = emailAddress;
-        }
-
-        public String getDeliveryAddress() {
-            return deliveryAddress;
-        }
-
-        public void setDeliveryAddress(String deliveryAddress) {
-            this.deliveryAddress = deliveryAddress;
-        }
-    }
-
     @Autowired
     private UserValidator userValidator;
     @PostMapping("/register")
